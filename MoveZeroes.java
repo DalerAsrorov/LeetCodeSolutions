2e372b8c48b04081b3f7441400411e82d9c68510
Daler Asrorov <asrorids@gmail.com>
2015-10-20 00:26:58 -0700

    Added solution to a new problem 'MoveZeroes'

---
+++ MoveZeroes.java
@@ -0,0 +1,23 @@
+package excersises;
+
+import java.util.Arrays;
+public class MoveZeroes {
+	public static void moveZeroes(int[] nums) {
+		int j = 0;
+		int value = 0;
+		for (int i = 0; i < nums.length; i++) {
+			value = nums[i];
+			for (int k = 0; k < nums.length - 1; k++)
+			{	
+				if (nums[i] == 0) {
+					for (j = i; j < nums.length - 1; j++) {
+						nums[j] = nums[j + 1];
+					}
+					nums[j] = 0;
+					//System.out.println(j);
+				}
+			}	
+		}
+		System.out.println(Arrays.toString(nums));
+	}
+}
